d3f5fb72a80b5847b0c3321e1342cb62a2bf0f6a
foxy82 <foxy82.github@gmail.com>
2020-07-24 16:23:09 +0100

    Fix pypi errors

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: Apache 2.0 License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3.7'
     ],
 )
